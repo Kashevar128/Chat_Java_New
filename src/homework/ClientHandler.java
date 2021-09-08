33bc1d502df6205afa1e07d7c5a502082bd5d152
Vinogradov_M <steve128@yandex.ru>
2021-09-08 17:29:37 +0300

    added annotations

--- src/homework/ClientHandler.java
+++ src/homework/ClientHandler.java
@@ -5,46 +5,47 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 
-public class ClientHandler implements Runnable {
+public class ClientHandler implements Runnable { //Класс обработчика клиентов реализующий интерфейс с методом run (Это нужно для задания созданному потоку).
 
-    private DataInputStream in;
-    private DataOutputStream out;
-    private boolean running;
+    //поля класса ClientHandler
+    private DataInputStream in; //поле входящего потока.
+    private DataOutputStream out; //поле исходящего потока.
+    private boolean running; // логический флаг, разрещаюший или запрещающий работать циклу в потоке run.
 
-    public ClientHandler(Socket socket) throws IOException {
-        in = new DataInputStream(socket.getInputStream());
-        out = new DataOutputStream(socket.getOutputStream());
-        running = true;
+    public ClientHandler(Socket socket) throws IOException { //создаем конструктор обработчика и передаем ему аргумент сокета - сущность содержащая инфу о порте и хосте.
+        in = new DataInputStream(socket.getInputStream()); //входящий поток извлекаемый из сокета кладется в переменную in в обертке класса DataInputStream для чтения сообщений в виде массивов байт.
+        out = new DataOutputStream(socket.getOutputStream()); //исходящий поток извелаемый из сокета кладется в переменную out в обертке класса DataOutputStream для передачи сообщений в виде массивов байт.
+        running = true; //флаг по умолчанию устанавилвается на правду.
     }
 
     public boolean isRunning() {
         return running;
-    }
+    } //геттер на running;
 
     public void setRunning(boolean running) {
         this.running = running;
+    } //сеттер на running;
+
+    public void sendMessage (String msg) throws IOException { //метод для отправки сообщений, с вложенным строковым аргументом.
+        out.writeUTF(msg); //пишем сообщение в поток out.
+        out.flush();//сброс накопленной информации в буфере в поток out, сообщение отправляется получателю.
     }
 
-    public void sendMessage (String msg) throws IOException {
-        out.writeUTF(msg);
-        out.flush();
-    }
-
-    @Override
-    public void run() {
-        while (running) {
-            try {
-                String msg = in.readUTF();
-                System.out.println("Message from client: " + msg);
-                if (msg.equals("/quit")) {
-                    out.writeUTF(msg);
-                    out.flush();
-                    running = false;
-                    break;
+    @Override // переопределение абстрактного метода run из интерфейса runnable.
+    public void run() { //тело метода run, указывает порядок действий дополнительному потоку.
+        while (running) { // цикл с булевым флагом.
+            try {                           //конструкция для обхода ошибок.
+                String msg = in.readUTF(); //читаем из потока входа сообщение в строковую переменную msg.
+                System.out.println("Message from client: " + msg); //вывод в консоль сообщения от клиента через переменную msg + приставака "Сообщение от клиента".
+                if (msg.equals("/quit")) { // если msg = "/quit" приходит от клиента, то
+                    out.writeUTF(msg); // это же сообщение отправляем обратно в поток out клиенту.
+                    out.flush(); // сбрасываем буфер.
+                    running = false; // булевую переменную переводим на "ложь", так как клиент вышел и поток чтения сообщений от клиента нужно прервать.
+                    break; // прерывание цикла while.
                 }
-            }catch (IOException e) {
-                System.out.println("Client kicked");
-                break;
+            }catch (IOException e) {                   //ошибка потоков входа-выхода,
+                System.out.println("Client kicked");   //в случае неправильного завершения работы клиента, пишет: "Клиент выкинут.".
+                break; //прерывание цилка while.
             }
         }
     }
--- src/homework/ioServer.java
+++ src/homework/ioServer.java
@@ -5,25 +5,25 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
 
-public class ioServer {
+public class ioServer { //класс сервера для приема и обработки клиентов.
 
-    private static ClientHandler handler;
+    private static ClientHandler handler; //поле(field) для экземпляра (инстанса) обработчка клиентов (клиентхендлер).
 
-    public static void main(String[] args) throws IOException {
-        System.out.println("Сервер запущен...");
-        Thread readThread = new Thread(() -> {
-            try {
-                Scanner in = new Scanner(System.in);
-                while (in.hasNext()) {
-                    String msg = in.next();
-                    if (handler == null) {
-                        System.out.println("You have to wait connection");
-                        while (handler == null) {
-                            Thread.sleep(500);
+    public static void main(String[] args) throws IOException { //основной метод для запуска программы сервера.
+        System.out.println("Сервер запущен..."); //запись в консоль.
+        Thread readThread = new Thread(() -> { //создание потока с run внутри в виде лямда-выражения. Поток отвечает за чтение записи в консоли и отправку его через екземпляр обработчика клиентов.
+            try {                              //конструкция для обхода ошибок.
+                Scanner in = new Scanner(System.in); //Создание экземпляра сканера in, на вход созданного сканера подается переменная in с типом данных InputStream, принадлежащая классу System.
+                while (in.hasNext()) { // метод hasNext() смотрит на наличие следущего токена в потоке, если он есть, то цикл работает.
+                    String msg = in.next(); // метод next() отвечает за пропуск строковых выражений в строковую переменную msg.
+                    if (handler == null) { // если handler пуст
+                        System.out.println("You have to wait connection"); // в консоль выводится просьба подождать подключения.
+                        while (handler == null) { // цикл работает, пока handler равен нулю.
+                            Thread.sleep(500); // в этом цикле поток будет погружаться в сон на 500 млс, после это проверять условие и опять засыпать.
                         }
                     }
-                    if (handler.isRunning()) {
-                        handler.sendMessage(msg);
+                    if (handler.isRunning()) { // если в хэндлере работает поток run
+                        handler.sendMessage(msg); //через метод sendMessage хэндлера отправляем сообщение.
                         if (msg.equals("/quit")) {
                             handler.setRunning(false);
                             break;
