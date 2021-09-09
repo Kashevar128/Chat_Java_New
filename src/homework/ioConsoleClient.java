c64a92fbf6519431e82727e596cc03515b419922
Vinogradov_M <steve128@yandex.ru>
2021-09-09 17:23:34 +0300

    added annotations 02

--- src/homework/ioConsoleClient.java
+++ src/homework/ioConsoleClient.java
@@ -5,20 +5,20 @@ import java.io.DataOutputStream;
 import java.net.Socket;
 import java.util.Scanner;
 
-public class ioConsoleClient {
-    public static void main(String[] args) {
+public class ioConsoleClient {  //класс клиента
+    public static void main(String[] args) { //основной метод программы клиента
         try {
-            Socket socket = new Socket("localhost", 8189);
-            DataInputStream in = new DataInputStream(socket.getInputStream());
-            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-            Thread writeThread = new Thread(() -> {
-                Scanner inS = new Scanner(System.in);
-                try {
-                    while (inS.hasNext()) {
-                        String msg = inS.next();
-                        out.writeUTF(msg);
-                        out.flush();
-                        if (msg.equals("/quit")) {
+            Socket socket = new Socket("localhost", 8189); // Создаем сокет, пишем Localhost, имея ввиду данный компьютер, пишем порт 8189
+            DataInputStream in = new DataInputStream(socket.getInputStream()); // Извекам исходящий поток из сокета, обертываем классом, конвертирующим сообщения в массив байт
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream()); // Тоже самое только с входящим потоком
+            Thread writeThread = new Thread(() -> { // Создаем поток для написания сообщений
+                Scanner inS = new Scanner(System.in); // Создаем сканер inS с вложенной переменной InputStream in
+                try {                                // Блок для обхода ошибок
+                    while (inS.hasNext()) { // Цикл работает пока есть на входе следующий токен
+                        String msg = inS.next(); // В строковую переменную msg кладем строки из переменной inS с помощью next
+                        out.writeUTF(msg); // В поток out кладем переменную msg с сообщением
+                        out.flush(); // Сбрасываем всю инфу из буфера
+                        if (msg.equals("/quit")) { // Если сообщение равно /quit, делаем прерывание цикла
                             break;
                         }
                     }
@@ -26,23 +26,23 @@ public class ioConsoleClient {
                     e.printStackTrace();
                 }
             });
-            Thread readThread = new Thread(() -> {
-                try {
-                    while (true) {
-                        String msg = in.readUTF();
-                        System.out.println("Message from server: " + msg);
-                        if (msg.equals("/quit")) {
-                            System.out.println("Program finished correctly");
-                            break;
+            Thread readThread = new Thread(() -> { // Создаем поток для чтения сообщений
+                try {                             // Блок для обхода ошибок
+                    while (true) {              // Цикл для чтений сообщений от сервера
+                        String msg = in.readUTF(); // В переменную читаем сообщение из потока in
+                        System.out.println("Message from server: " + msg); // Пишем в консоль сообщение от сервера.
+                        if (msg.equals("/quit")) { // если сообщение равно /quit
+                            System.out.println("Program finished correctly"); // Пишем что программа завершена корректно
+                            break; // Делаем прерываение цикла
                         }
                     }
                 } catch (Exception e) {
-                    System.out.println("Server was broken");
+                    System.out.println("Server was broken"); // Если сервер пропал, пишем в консоль, что сервер был уничтожен
                 }
             });
-            writeThread.setDaemon(true);
-            writeThread.start();
-            readThread.start();
+            writeThread.setDaemon(true); // Делаем пишущий поток демоном
+            writeThread.start(); // Запускаем пишущий поток
+            readThread.start(); // Запускаем читающий поток
         } catch (Exception e) {
             e.printStackTrace();
         }
